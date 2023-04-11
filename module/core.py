cb43983c869ae7f961b09d2a1ce2df7d43dfa067
Leon Scheidweiler <leon.scheidweiler@gmail.com>
2023-04-11 16:57:42 +0200

    provide l1b_file as command line argument to core.py

--- module/core.py
+++ module/core.py
@@ -19,7 +19,11 @@ atm_data = atm.prepare_atm()
 
 # gather (lat, lon) grid from l1b file
 # gather time information from l1b file
-l1b.import_l1b_grid_example(atm_data, "data/input/L1B_example.nc")
+l1b_file = sys.argv[1]
+l1b_name = l1b_file.split("/")[-1]
+if not l1b_name.startswith("L1B_"):
+    sys.exit("L1B file name has to start with L1B_")
+l1b.import_l1b_grid_example(atm_data, l1b_file)
 
 # gather surface elevation from aster file
 # the spatial grid is interpolated onto the l1b spatial grid
@@ -37,5 +41,7 @@ atm_data = era5.main(atm_data, "data/external/era5/download")
 # the temporal grid is interpolated onto the l1b temporal grid
 atm_data = cams.main(atm_data, "data/external/cams/download")
 
+atm_name = "ATM_" + l1b_name.strip("L1B_")
 print(f"finished atm_data = {atm_data}")
-atm_data.to_netcdf("data/output/ATM_example.nc", mode = "w", format = "NETCDF4")
+print(f"saving to {atm_name}")
+atm_data.to_netcdf(f"data/output/{atm_name}", mode = "w", format = "NETCDF4")
