04bb720a4382c744dbd4bfc3c87901cc45e997d3
Leonie Scheidweiler <leonie.olivia.scheidweiler@gmail.com>
2025-05-26 14:42:10 +0200

    adjusted memory needed for preprocessing

--- PreProc_ATM/slurm_job.sh
+++ PreProc_ATM/slurm_job.sh
@@ -2,7 +2,7 @@
 #SBATCH --partition=cpu-single         # Partition (change if needed)
 #SBATCH --ntasks=1                     # Number of tasks (1 process)
 #SBATCH --time=01:00:00                # Time limit (HH:MM:SS)
-#SBATCH --mem=32G                      # Memory required
+#SBATCH --mem=200G                     # Memory required
 #SBATCH --job-name=PreProc_ATM         # Job name
 #SBATCH --output=slurm/preproc/atm.log # Log file (stores stdout/stderr)
 #SBATCH --error=slurm/preproc/atm.log  # Error file (if errors occur)
--- PreProc_L1B/slurm_job.sh
+++ PreProc_L1B/slurm_job.sh
@@ -2,7 +2,7 @@
 #SBATCH --partition=cpu-single         # Partition (change if needed)
 #SBATCH --ntasks=1                     # Number of tasks (1 process)
 #SBATCH --time=01:00:00                # Time limit (HH:MM:SS)
-#SBATCH --mem=32G                      # Memory required
+#SBATCH --mem=200G                      # Memory required
 #SBATCH --job-name=PreProc_L1B         # Job name
 #SBATCH --output=slurm/preproc/l1b.log # Log file (stores stdout/stderr)
 #SBATCH --error=slurm/preproc/l1b.log  # Error file (if errors occur)
